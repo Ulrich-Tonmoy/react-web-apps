9a828134007334043e49242a98a8bcf1a36f984b
Tonmoy <ulrichtonmoy1@gmail.com>
2023-01-12 02:02:16 +0600

    removed

--- templates-2/redux-toolkit-blog-rtk-query/src/features/api/apiSlice.js
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-export const apiSlice = createApi({
-    reducerPath: "api", // optional
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
-    tagTypes: ["Post", "User"],
-    endpoints: (builder) => ({}),
-});
