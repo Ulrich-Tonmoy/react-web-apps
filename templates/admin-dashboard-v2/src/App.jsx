f8a26ed7194e1e51bd073332d75b57032260d47d
Tonmoy <ulrichtonmoy1@gmail.com>
2023-02-10 01:25:23 +0600

    init

---
+++ templates/admin-dashboard-v2/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <div className="app"></div>
+        </Router>
+    );
+}
+
+export default App;
