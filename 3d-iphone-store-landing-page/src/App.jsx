358fd31f3f7de2dcec37ff80d5b1fb7e1d03ff6d
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-28 01:28:22 +0600

    💄 sound section

--- 3d-iphone-store-landing-page/src/App.jsx
+++ 3d-iphone-store-landing-page/src/App.jsx
@@ -1,11 +1,13 @@
 import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Navbar";
+import SoundSection from "./components/SoundSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Jumbotron />
+      <SoundSection />
     </div>
   );
 }
