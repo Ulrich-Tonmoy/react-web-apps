71bbd8efddbace01ceb4a789bed1e4e57ad9797a
Ulrich <ulrichtonmoy1@gmail.com>
2024-06-29 03:12:24 +0600

    destroy wall fixed not destroying correctly

--- pathfinding-visualizer/src/libs/utils/destroyWall.ts
+++ pathfinding-visualizer/src/libs/utils/destroyWall.ts
@@ -10,8 +10,8 @@ export const destroyWall = async (
   speed: SpeedType,
 ) => {
   if (isRight && grid[row][col + 1]) {
-    grid[row][col].isWall = false;
-    document.getElementById(`${row}-${col}`)!.className = TILE_STYLE;
+    grid[row][col + 1].isWall = false;
+    document.getElementById(`${row}-${col + 1}`)!.className = TILE_STYLE;
     await sleep(20 * SPEEDS.find((s) => s.value === speed)!.value - 5);
   } else if (grid[row + 1]) {
     grid[row + 1][col].isWall = false;
