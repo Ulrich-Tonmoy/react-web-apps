a5c528d2bc71bdb27a6b18f4dc9515841464bc51
Ulrich <ulrichtonmoy1@gmail.com>
2024-07-11 03:47:51 +0600

    home layout created

--- sorting-visualizer/src/components/Home.tsx
+++ sorting-visualizer/src/components/Home.tsx
@@ -1,13 +1,34 @@
 import { useSortingAlgorithmContext } from "@/libs";
-import { useEffect } from "react";
 
 export const Home = () => {
   const { arrayToSort, isSorting } = useSortingAlgorithmContext();
 
-  useEffect(() => {
-    console.log("🚀 ~ Home ~ isSorting:", isSorting);
-    console.log("🚀 ~ Home ~ arrayToSort:", arrayToSort);
-  }, []);
-
-  return <div>Home</div>;
+  return (
+    <div className="absolute top-0 h-screen w-screen z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
+      <div className="flex justify-center h-full">
+        <div
+          id="content-container"
+          className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4"
+        >
+          <div className="h-[66px] relative flex items-center justify-between w-full">
+            <h1 className="hidden text-2xl font-light text-gray-300 md:flex">
+              Sorting Visualizer
+            </h1>
+            <div className="">Controls</div>
+          </div>
+          <div className="relative h-[calc(100vh-66px)] w-full">
+            <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
+              {arrayToSort.map((value, index) => (
+                <div
+                  key={index}
+                  className="array-line relative w-1 mx-0.5 shadow-lg opacity-70 rounded-lg default-line-color"
+                  style={{ height: `${value}px` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
